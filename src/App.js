9ca0865a6fc5fc4adc44364a7cca8b0caf5f9c43
Your Name <you@example.com>
2023-05-29 12:49:34 +0500

    almost completed!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from './components/form';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/form";
 
 function App() {
+  const [calculatedTime, setCalculatedTime] = useState({
+    days: 0,
+    months: 0,
+    years: 0,
+  });
   return (
     <div className="App">
-      <div className='card_main_container'>
-      <Form />
-      <h1><span>--</span>years</h1>
-      <h1><span>--</span> months</h1>
-      <h1><span>--</span>days</h1>
+      <div className="card_main_container">
+        <Form setCalculatedTime={setCalculatedTime} />
+        <h1>
+          <span>{calculatedTime.years > 0 ? calculatedTime.years : "--"}</span>
+          years
+        </h1>
+        <h1>
+          <span>
+            {calculatedTime.months > 0 ? calculatedTime.months : "--"}
+          </span>{" "}
+          months
+        </h1>
+        <h1>
+          <span>{calculatedTime.days > 0 ? calculatedTime.days : "--"}</span>
+          days
+        </h1>
       </div>
     </div>
   );
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,25 +1,46 @@
-import React from "react"
-import "./index.css"
-import ArrowIcon from '../../images/icon-arrow.svg'
+import React from "react";
+import "./index.css";
+import ArrowIcon from "../../images/icon-arrow.svg";
+import { useForm } from "react-hook-form";
 
-export default function Form() {
+export default function Form({ setCalculatedTime }) {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    const today = new Date();
+    let days = 0;
+    let months = 0;
+    let years = 0;
+    days = today.getDate() - data.day;
+    months = today.getMonth() + 1 - data.month;
+    years = today.getFullYear() - data.year;
+    setCalculatedTime({
+      days,
+      months,
+      years,
+    });
+  };
+
   return (
     <div>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="input_main_container">
           <label className="label">day</label>
-          <input className="input" />
+          <input
+            type="number"
+            className="input"
+            {...register("day", {required: true, max: 31 })}
+          />
         </div>
         <div className="input_main_container ml">
           <label className="label">month</label>
-          <input className="input" />
+          <input className="input" {...register("month")} type="number" />
         </div>
         <div className="input_main_container  ml">
           <label className="label">year</label>
-          <input className="input" />
+          <input className="input" {...register("year")} type="number" />
         </div>
         <button>
-         <img src={ArrowIcon} alt='ArrowIcon' width={40} height={40}/>
+          <img src={ArrowIcon} alt="ArrowIcon" width={40} height={40} />
         </button>
       </form>
     </div>
